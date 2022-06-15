1ee026ab9758cc2604084f740d642ce29a03b494
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2022-06-14 23:59:05 -0300

    adding base project

---
+++ Fiero/FieroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieroApp.swift
+//  Fiero
+//
+//  Created by Marina De Pazzi on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FieroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
