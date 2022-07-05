0af5bcc1fadd9f5babc3c914e8d8245b83547188
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2022-07-05 16:14:49 -0300

    feat: finish textfield component

--- Fiero/Views/DesignSystem/Components/PrimaryTextField.swift
+++ Fiero/Views/DesignSystem/Components/PrimaryTextField.swift
@@ -8,17 +8,11 @@
 import Foundation
 import SwiftUI
 
-//public protocol TextFieldStyle {
-//    associatedtype _Body = View
-//    @ViewBuilder func _body(configuration: TextField<Self._Label>) -> Self._Body
-//    typealias _Label = _TextFieldStyleLabel
-//}
-
 struct PrimaryTextFieldStyle: TextFieldStyle {
     func _body(configuration: TextField<_Label>) -> some View {
         configuration
             .multilineTextAlignment(.leading)
-            .font(.system(size: Tokens.Fonts.Size.xs.value, weight: Tokens.Fonts.Weight.regular.value, design: Tokens.Fonts.Familiy.sfprodisplay.value))
+            .font(.system(size: Tokens.Fonts.Size.xs.value, weight: Tokens.Fonts.Weight.regular.value, design: Tokens.Fonts.Familiy.support.value))
             .foregroundColor(Tokens.Colors.Neutral.High.pure.value)
             .padding(Tokens.Spacing.xxxs.value)
             .frame(maxWidth: .infinity)
@@ -27,42 +21,18 @@ struct PrimaryTextFieldStyle: TextFieldStyle {
     
     var borderView: some View {
         RoundedRectangle(cornerRadius: Tokens.Border.BorderRadius.small.value)
-            .stroke(lineWidth: Tokens.Border.BorderWidth.hairline.value)
-            .background(RoundedRectangle(cornerRadius: Tokens.Border.BorderRadius.small.value).foregroundColor(Tokens.Colors.Neutral.Low.pure.value))
+            .stroke(Tokens.Colors.Neutral.High.pure.value, lineWidth: Tokens.Border.BorderWidth.thin.value)
     }
-}
-
-struct PrimaryTextFieldView<Content> : View where Content: View {
-    var text: String
-    var placeholder: String
-    var content: Content
-
-    init(text: String, placeholder: String, @ViewBuilder content: () -> Content) {
-        self.text = text
-        self.placeholder = placeholder
-        self.content = content()
-    }
-
-    var body: some View {
-        TextField("TitleKey", text: .constant("Text"))
-            .textFieldStyle(.plain)
-            .multilineTextAlignment(.center)
-            .font(.system(size: Tokens.Fonts.Size.xg.value, weight: Tokens.Fonts.Weight.black.value, design: Tokens.Fonts.Familiy.sfprodisplay.value))
-            .frame(maxWidth: .infinity)
-            .background(
-                RoundedRectangle(cornerRadius: Tokens.Border.BorderRadius.small.value)
-                    .stroke(lineWidth: Tokens.Border.BorderWidth.hairline.value)
-                    .foregroundColor(Tokens.Colors.Neutral.High.pure.value)
-            )
-    }
-
 }
 
 struct FlemisView: View {
     var body: some View {
-        TextField("Flemis", text: .constant("Flemis"))
-            .textFieldStyle(PrimaryTextFieldStyle())
-            .padding(Tokens.Spacing.xs.value)
+        VStack {
+            TextField("Flemis", text: .constant("Flemis"))
+                .textFieldStyle(PrimaryTextFieldStyle())
+                .padding(Tokens.Spacing.xs.value)
+        }
+        .background(Color.black)
     }
 }
 
@@ -71,3 +41,4 @@ struct FlemisView_Previews: PreviewProvider {
         FlemisView()
     }
 }
+
