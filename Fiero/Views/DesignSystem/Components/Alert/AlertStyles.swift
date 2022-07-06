d91ef27f61780f383c154d08be4b95aac9ac3250
jgabrielbiazus <j.gquevedo50@gmail.com>
2022-07-06 16:50:01 -0300

    fix: merge branch/dev

--- Fiero/Views/DesignSystem/Components/Alert/AlertStyles.swift
+++ Fiero/Views/DesignSystem/Components/Alert/AlertStyles.swift
@@ -15,34 +15,34 @@ enum AlertStyle {
     var backgroundColor: Color {
         switch self {
         case .primary:
-            return Tokens.Colors.Neutral.High.pure.color
+            return Tokens.Colors.Neutral.High.pure.value
         case .secondary:
-            return Tokens.Colors.Brand.Primary.pure.color
+            return Tokens.Colors.Brand.Primary.pure.value
         }
     }
     var borderRadius: CGFloat {
-        return Tokens.Border.BorderRadius.small.borderRadius
+        return Tokens.Border.BorderRadius.small.value
     }
     
     //MARK: Font variables
     var fontSize: CGFloat {
-        return Tokens.Fonts.Size.xs.size
+        return Tokens.Fonts.Size.xs.value
     }
     var fontColor: Color {
         switch self {
         case .primary:
-            return Tokens.Colors.Neutral.Low.pure.color
+            return Tokens.Colors.Neutral.Low.pure.value
         case .secondary:
-            return Tokens.Colors.Neutral.High.pure.color
+            return Tokens.Colors.Neutral.High.pure.value
         }
     }
     var fontFamily: Font.Design {
-        return Tokens.Fonts.Familiy.sfprodisplay.family
+        return Tokens.Fonts.Familiy.support.value
     }
     var fontWeight: Font.Weight {
-        return Tokens.Fonts.Weight.regular.weight
+        return Tokens.Fonts.Weight.regular.value
     }
     var padding: Double{
-        return Tokens.Spacing.nano.spacing
+        return Tokens.Spacing.nano.value
     }
 }
