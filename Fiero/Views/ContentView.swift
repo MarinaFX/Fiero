6b30bfab5b758e36ae7d3a19d81e067c85c2b123
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2022-07-07 18:09:33 -0300

    feat: develop account login screen

--- Fiero/Views/ContentView.swift
+++ Fiero/Views/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CustomTextFieldView(type: .none, style: .secondary, placeholder: "fodase", helperText: "CANSADONA CANSADONA CAN SA DO NA. QUANDO CHEGA O FINAL DE SEMANA, QUERO DEITAR NA MINHA CAMA", isWrong: .constant(true), text: .constant("flemis"))
-            .padding(Tokens.Spacing.xs.value)
+        AccountLoginView()
     }
 }
 
