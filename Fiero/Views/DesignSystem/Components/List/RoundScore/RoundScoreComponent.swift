f805b2128c6aec37271c5ecd8659ca84eac8834d
jgabrielbiazus <j.gquevedo50@gmail.com>
2022-07-15 16:41:17 -0300

    feat: RoundScoreCompoment

---
+++ Fiero/Views/DesignSystem/Components/List/RoundScore/RoundScoreComponent.swift
@@ -0,0 +1,28 @@
+//
+//  RoundScoreComponent.swift
+//  Fiero
+//
+//  Created by João Gabriel Biazus de Quevedo on 15/07/22.
+//
+
+import SwiftUI
+
+struct RoundScoreComponent: View {
+//    @ScaledMetric(relativeTo: .caption) var spacing: CGFloat = 100
+    @State var style: RoundScoreStyle
+    
+    var body: some View {
+        ZStack {
+            Image(systemName: style.icon)
+                .font(style.iconFont)
+                .foregroundColor(style.backgroundColor)
+                .background(style.iconColor)
+                .clipShape(Circle())
+        }
+    }
+}
+struct RoundScoreComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundScoreComponent(style: .lose)
+    }
+}
