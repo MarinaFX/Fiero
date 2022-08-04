8e8607569ba3548174a68f42e6b32821b9041e20
jgabrielbiazus <j.gquevedo50@gmail.com>
2022-08-04 16:55:43 -0300

    feat: DuelScoreController

---
+++ Fiero/Views/DesignSystem/Components/ScoreControllers/ScoreControllerDuel/ScoreControllerDuel.swift
@@ -0,0 +1,73 @@
+//
+//  ScoreControllerDuel.swift
+//  Fiero
+//
+//  Created by João Gabriel Biazus de Quevedo on 04/08/22.
+//
+
+import SwiftUI
+
+struct teste: View {
+    
+    @State var style: ScoreControllerStyle
+    @State var maxValue: Int
+    @State var count: Int = 0
+    @State var playerName: String
+    
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: style.borderRadius)
+                .foregroundColor(style.backgroundColor)
+            
+            VStack(alignment:.center ) {
+                HStack() {
+                    Button(action: {
+                        if count > 0{
+                            count -= 1
+                        }
+                    }) {
+                        Image(systemName: style.minusIcon)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(style.buttonColor)
+                        
+                    }
+                    .padding(.leading, style.spacing)
+                    
+                    Spacer()
+                    
+                    Text("\(count)")
+                        .foregroundColor(style.buttonColor)
+                        .font(.system(size: 34))
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        if count < maxValue{
+                            count += 1
+                        }
+                    }) {
+                        Image(systemName: style.plusIcon)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(style.buttonColor)
+                    }
+                    .padding(.trailing, style.spacing)
+                }
+                .padding(.vertical, style.spacingVertical)
+                
+                Text("\(playerName)")
+                    .foregroundColor(style.buttonColor)
+                    .font(.system(size: 24))
+                    .padding(.horizontal, style.spacingVertical)
+            }
+        }
+        .frame(width: 285, height: 120)
+    }
+}
+struct teste_Previews: PreviewProvider {
+    static var previews: some View {
+        teste(style: .first, maxValue: 10, playerName: "Alpaca Enfurecida")
+    }
+}
---
+++ Fiero/Views/DesignSystem/Components/ScoreControllers/ScoreControllerDuel/ScoreControllerStyle.swift
@@ -0,0 +1,58 @@
+//
+//  ScoreControllerDuelStyle.swift
+//  Fiero
+//
+//  Created by João Gabriel Biazus de Quevedo on 04/08/22.
+//
+
+import SwiftUI
+
+enum ScoreControllerStyle {
+    case first
+    case second
+    
+    //MARK: - Colors
+    var backgroundColor: Color {
+        switch self {
+        case .first:
+            return Tokens.Colors.Background.dark.value.opacity(0.9)
+        case .second:
+            return Tokens.Colors.Neutral.High.pure.value
+        }
+    }
+    var buttonColor: Color {
+        switch self {
+        case .first:
+            return Tokens.Colors.Neutral.High.pure.value
+        case .second:
+            return Tokens.Colors.Background.dark.value
+        }
+    }
+    //MARK: - Fonts
+    var textFont: Font {
+        return Tokens.FontStyle.largeTitle.font(weigth: .regular)
+    }
+    var numberFont: Font {
+        return Tokens.FontStyle.largeTitle.font(weigth: .bold)
+    }
+    
+    //MARK: - Radius
+    var borderRadius: CGFloat {
+        return Tokens.Border.BorderRadius.small.value
+    }
+    
+    //MARK: - Icons
+    var plusIcon: String {
+        return "plus.circle.fill"
+    }
+    var minusIcon: String {
+        return "minus.circle.fill"
+    }
+    
+    var spacing: Double{
+        return Tokens.Spacing.xxs.value
+    }
+    var spacingVertical: Double{
+        return Tokens.Spacing.nano.value
+    }
+}
