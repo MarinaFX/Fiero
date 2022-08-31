1153925a446a9357bc19544182b91c8050405415
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-08-31 10:58:35 -0300

    create lottie struct to call animations

---
+++ Fiero/Lottie.swift
@@ -0,0 +1,40 @@
+//
+//  Lottie.swift
+//  Fiero
+//
+//  Created by Marcelo Diefenbach on 31/08/22.
+//
+
+import Foundation
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    
+    typealias UIViewType = UIView
+    var fileName: String
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        let animationView = AnimationView()
+        let animation = Animation.named(fileName)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+}
