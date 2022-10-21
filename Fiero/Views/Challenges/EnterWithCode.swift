1574b320f1bea126dc078ce680b7c2270e3598fa
illuminaty <naty.brocca@gmail.com>
2022-10-21 16:48:25 -0300

    fix: add navigation to EnterWithCodeView

--- Fiero/Views/Challenges/EnterWithCode.swift
+++ Fiero/Views/Challenges/EnterWithCode.swift
@@ -12,35 +12,39 @@ struct EnterWithCode: View {
     @State private var challengeCode: String = ""
     
     var body: some View {
-        ZStack {
-            Tokens.Colors.Background.dark.value
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack(spacing: Tokens.Spacing.xs.value) {
-                Spacer()
+        NavigationView {
+            ZStack {
+                Tokens.Colors.Background.dark.value
+                    .edgesIgnoringSafeArea(.all)
                 
-                Text(LocalizedStringKey("enterWithCodeDescription"))
-                    .multilineTextAlignment(.center)
-                    .font(Tokens.FontStyle.largeTitle.font(weigth: .bold))
-                    .foregroundColor(Tokens.Colors.Neutral.High.pure.value)
-                    .padding(.horizontal, Tokens.Spacing.lg.value)
-                
-                VStack(spacing: Tokens.Spacing.xxxs.value) {
-                    CustomTextFieldView(placeholder: "enterWithCodePlaceholder",
-                                        keyboardType: .alphabet,
-                                        isSecure: false,
-                                        isLowCase: true,
-                                        isWrong: .constant(false),
-                                        text: $challengeCode)
+                VStack(spacing: Tokens.Spacing.xs.value) {
+                    Spacer()
+                    
+                    Text(LocalizedStringKey("enterWithCodeDescription"))
+                        .multilineTextAlignment(.center)
+                        .font(Tokens.FontStyle.largeTitle.font(weigth: .bold))
+                        .foregroundColor(Tokens.Colors.Neutral.High.pure.value)
+                        .padding(.horizontal, Tokens.Spacing.lg.value)
+                    
+                    VStack(spacing: Tokens.Spacing.xxxs.value) {
+                        CustomTextFieldView(placeholder: "enterWithCodePlaceholder",
+                                            keyboardType: .alphabet,
+                                            isSecure: false,
+                                            isLowCase: true,
+                                            isWrong: .constant(false),
+                                            text: $challengeCode)
 
-                    ButtonComponent(style: .primary(isEnabled: true),
-                                    text: "enterWithCodeButtonText") {
-                        print("enter the challenge")
+                        ButtonComponent(style: .primary(isEnabled: true),
+                                        text: "enterWithCodeButtonText") {
+                            print("enter the challenge")
+                        }
                     }
+                    .padding(.horizontal, Tokens.Spacing.defaultMargin.value)
+                    .padding(.bottom, Tokens.Spacing.sm.value)
                 }
-                .padding(.horizontal, Tokens.Spacing.defaultMargin.value)
-                .padding(.bottom, Tokens.Spacing.sm.value)
             }
+            .navigationTitle(LocalizedStringKey("enterWithCodeNavTitle"))
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
