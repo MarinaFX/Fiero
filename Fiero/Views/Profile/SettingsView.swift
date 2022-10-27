08fd30738e9d1fab5920fbd1d208061f6041e406
jgabrielbiazus <j.gquevedo50@gmail.com>
2022-10-27 14:54:25 -0300

    fix: Move buttons to settingsView

--- Fiero/Views/Profile/SettingsView.swift
+++ Fiero/Views/Profile/SettingsView.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SettingsView: View {
     
     @Environment(\.presentationMode) var presentationMode
     
     @EnvironmentObject var userViewModel: UserViewModel
+    
+    @State private var subscriptions: Set<AnyCancellable> = []
     
     var body: some View {
         NavigationView {
@@ -34,6 +37,63 @@ struct SettingsView: View {
                 Button("Feito!") {
                     self.presentationMode.wrappedValue.dismiss()
                 }
+            }
+        }
+        .alert(isPresented: $userViewModel.showingAlert) {
+            switch self.userViewModel.activeAlert {
+                case .error:
+                    return Alert(
+                        title: Text("Oops, muito desafiador!"),
+                        message: Text("Não conseguimos excluir sua conta no momento, tente mais tarde."),
+                        dismissButton: .default(Text("OK")){
+                            self.userViewModel.showingAlertToFalse()
+                        }
+                    )
+                case .confirmAccountDelete:
+                    return Alert(
+                        title: Text("Apagar conta"),
+                        message: Text("Essa ação não poderá ser desfeita."),
+                        primaryButton: .destructive(Text("Apagar minha conta")) {
+                            self.userViewModel.deleteAccount()
+                                .sink(receiveCompletion: { completion in
+                                    switch completion {
+                                        case .finished:
+                                            print("Successfully shared delete account subscriptions to ProfileView")
+                                        case .failure(_):
+                                            self.userViewModel.activeAlert = .error
+                                            self.userViewModel.showingAlertToTrue()
+                                    }
+                                }, receiveValue: {
+                                    self.userViewModel.showingAlertToFalse()
+                                    self.userViewModel.cleanDefaults()
+                                    self.userViewModel.isLogged = false
+                                })
+                                .store(in: &subscriptions)
+                        },
+                        secondaryButton: .cancel(Text("Cancelar")){
+                            self.userViewModel.showingAlertToFalse()
+                        }
+                    )
+                case .logOut:
+                    return Alert(
+                        title: Text("Sair da conta"),
+                        message: Text(""),
+                        primaryButton: .destructive(Text("Sair da conta")) {
+                            self.userViewModel.cleanDefaults()
+                            self.userViewModel.isLogged = false
+                        },
+                        secondaryButton: .cancel(Text("Cancelar")){
+                            self.userViewModel.showingAlertToFalse()
+                        }
+                    )
+                case .none:
+                    return Alert(
+                        title: Text("Oops, muito desafiador!"),
+                        message: Text("Não conseguimos excluir sua conta no momento, tente mais tarde."),
+                        dismissButton: .default(Text("OK")){
+                            self.userViewModel.showingAlertToFalse()
+                        }
+                    )
             }
         }
     }
