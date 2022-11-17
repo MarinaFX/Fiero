2c219ea6ba4e62ad0208ea7abdc8a8a2b570a6c3
jgabrielbiazus <j.gquevedo50@gmail.com>
2022-11-16 22:19:15 -0300

    feat: ScoreBoardList Component

---
+++ Fiero/Views/DesignSystem/Components/ScoreList/ScoreList.swift
@@ -0,0 +1,20 @@
+//
+//  ScoreList.swift
+//  Fiero
+//
+//  Created by João Gabriel Biazus de Quevedo on 16/11/22.
+//
+
+import SwiftUI
+
+struct ScoreList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScoreList_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreList()
+    }
+}
---
+++ Fiero/Views/DesignSystem/Components/ScoreList/ScoreListStyle.swift
@@ -0,0 +1,8 @@
+//
+//  ScoreListStyle.swift
+//  Fiero
+//
+//  Created by João Gabriel Biazus de Quevedo on 16/11/22.
+//
+
+import Foundation
